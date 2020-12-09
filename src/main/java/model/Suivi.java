00b1a637e7d79c0f47183079e30b7f1b3b2cdbcf
hajar <qaibouhajar@gmail.com>
2020-12-09 17:34:51 +0100

    commit classe suivi

---
+++ src/main/java/model/Suivi.java
@@ -0,0 +1,50 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Suivi {
+	@Id
+	private Integer Id;
+	private boolean accepte;
+	@ManyToOne
+	private Utilisateur follower;
+	@ManyToOne
+	private Utilisateur following;
+	
+	public Suivi() {
+	}
+	public Suivi(boolean accepte, Utilisateur follower, Utilisateur following) {
+		this.accepte = accepte;
+		this.follower = follower;
+		this.following = following;
+	}
+	public Integer getId() {
+		return Id;
+	}
+	public void setId(Integer id) {
+		Id = id;
+	}
+	public boolean isAccepte() {
+		return accepte;
+	}
+	public void setAccepte(boolean accepte) {
+		this.accepte = accepte;
+	}
+	public Utilisateur getFollower() {
+		return follower;
+	}
+	public void setFollower(Utilisateur follower) {
+		this.follower = follower;
+	}
+	public Utilisateur getFollowing() {
+		return following;
+	}
+	public void setFollowing(Utilisateur following) {
+		this.following = following;
+	}
+
+
+}
