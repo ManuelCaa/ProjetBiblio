42d9e7c221f6e0f4190886345c66b03d6fdf7f26
Briffault <banonmagali@yahoo.fr>
2020-12-10 17:14:47 +0100

    Commit Test JPA

--- src/main/java/test/TestJPA.java
+++ src/main/java/test/TestJPA.java
@@ -40,18 +40,44 @@ public class TestJPA {
 		return sc.nextLine();
 	}
 	public static void remplissageBase() {
-			Bibliotheque b =new Bibliotheque();
-			
-			Bibliotheque hajar =new Bibliotheque();
+		
+			Bibliotheque b1 =new Bibliotheque();			
+			Bibliotheque b2 =new Bibliotheque();
 
-			Admin a = new Admin("admin@gmail,com","123456","admin",b);
-			Admin a1=new Admin("admin1@gmail.com","toto","admin1",hajar);
+			Admin a = new Admin("admin@gmail,com","123456","admin",b1);
+			Admin a1=new Admin("admin1@gmail.com","toto","admin1",b2);
 			
-			Utilisateur u1 = new Utilisateur("hajar@gmail.com", "1234567", "hajars", hajar);
-			Utilisateur u2 = new Utilisateur("toto@gmail.com", "totol", "u2", b);
+			Utilisateur u1 = new Utilisateur("hajar@gmail.com", "1234567", "hajars", b1);
+			Utilisateur u2 = new Utilisateur("toto@gmail.com", "totol", "u2", b2);
+			
+			Oeuvre o1=new Oeuvre("Harry Potter et la coupe de feu","quatrième année au collège de Poudlard","HPCF",
+					2000,"Gallimard",LocalDate.parse("11-05-2018"), LocalDate.parse("12-04-2019"),true);
+			
+			Oeuvre o2=new Oeuvre("Harry Potter à l'école des sorciers","première année au collège de Poudlard","HPES",
+					1998,"Gallimard",LocalDate.parse("25-08-2012"),LocalDate.parse("12-04-2019"),true);
+			
+			Album al1 = new Album("Album1","album1 sur scène","FIAL1",2005, "Universal",LocalDate.parse("26-08-2014"),LocalDate.parse("25-04-2018"),true,12,"M");
+			Album al2 = new Album("Album2","album2 compil","FIAL2",2007,"Universal",LocalDate.parse("26-12-2017"),LocalDate.parse("05-06-2019"),true,14,"Goya");
+			
+			Livre l1=new Livre("Harry Potter à l'école des sorciers","première année au collège de Poudlard","HPES",
+					1998,"Gallimard",LocalDate.parse("25-08-2012"),LocalDate.parse("12-04-2019"),true,15,"Rowling");
+			
+			Livre l2= new Livre("Harry Potter et le prisionner d'Azkaban","troisième année au collège de Poudlard","HPPA",
+					1999,"Gallimard",LocalDate.parse("19-08-2014"),LocalDate.parse("12-04-2019"),true,18,"Rowling");
+			
 			
 			Context.getInstance().getDaoAdmin().insert(a);
 			Context.getInstance().getDaoAdmin().insert(a1);
+			Context.getInstance().getDaoUtilisateur().insert(u1);
+			Context.getInstance().getDaoUtilisateur().insert(u1);
+			Context.getInstance().getDaoBibliotheque().insert(b1);
+			Context.getInstance().getDaoBibliotheque().insert(b2);
+			Context.getInstance().getDaoOeuvre().insert(o1);
+			Context.getInstance().getDaoOeuvre().insert(o2);
+			Context.getInstance().getDaoAlbum().insert(al1);
+			Context.getInstance().getDaoAlbum().insert(al2);
+			Context.getInstance().getDaoLivre().insert(l1);
+			Context.getInstance().getDaoLivre().insert(l2);
 			
 			Context.getInstance().getDaoAdmin().update(a).setMail("admin2@gmail.com");
 		
